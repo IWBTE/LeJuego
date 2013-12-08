8598146cd4f46ff18e3ffaa6d79a46c42ad7c2ed
IWBTE <iwannabetheengineer@gmail.com>
2013-12-08 01:35:19 -0300

    VichoLovers Dying
    Ahora las pelas se mueren, aunque aun no matan :c

--- IWBTE/Proyecto 1/CobreloaLoMasGrande.py
+++ IWBTE/Proyecto 1/CobreloaLoMasGrande.py
@@ -4,6 +4,8 @@ import os
 import sys
 from random import uniform
 from random import randint
+from threading import Timer
+import time
 
 # -----------
 # Constantes
@@ -51,6 +53,12 @@ class Bala(pygame.sprite.Sprite):
         self.rect.centery =vichopls.rect.centery
         self.dir = dir
         self.mov = True
+
+    def tunazo(self, baleado):
+        if self.rect.colliderect(baleado.rect):
+            return True
+
+
     def mover(self):
         if len(self.dir)==1:
             if "u" in self.dir:
@@ -123,19 +131,17 @@ class Bala(pygame.sprite.Sprite):
                     self.kill()
                     self.mov = False
         
-        
-        
-        
-
-
+                       
 class Vicho(pygame.sprite.Sprite):
 
     def __init__(self, x,imag):
         pygame.sprite.Sprite.__init__(self)
+        self.asesinatos = 0
         self.image = load_image(imag, IMG_DIR, alpha=True)
         self.rect = self.image.get_rect()
         self.rect.centerx = x
         self.rect.centery = SCREEN_HEIGHT / 2
+        self.contadorBalas = 0
 
     def humano(self):
         # Controlar que la paleta no salga de la pantalla
@@ -149,7 +155,36 @@ class Vicho(pygame.sprite.Sprite):
             self.rect.right = SCREEN_WIDTH
 
 
+class vichoLover(pygame.sprite.Sprite):
+    def __init__(self,imag):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = load_image(imag, IMG_DIR,alpha=True)
+        num = randint(0,1)
+        self.rect = self.image.get_rect()
+        self.rect.centerx=SCREEN_WIDTH-50
+        self.rect.centery=SCREEN_HEIGHT*num
+        self.hp = 30
+        self.vivo = True
 
+    def mover(self, objetivo):
+        if objetivo.rect.centerx - self.rect.centerx >= 0:
+            self.rect.centerx += 1
+        else:
+            self.rect.centerx -= 1
+        if objetivo.rect.centery - self.rect.centery >= 0:
+            self.rect.centery += 1
+        else:
+            self.rect.centery -= 1 
+
+class daGame:
+    def __init__(self):
+        self.balas=[]
+        self.ultimo = "r"
+        self.lovers = []
+        self.enemies = 0
+        self.tiempoActual = 0
+        self.spawn = 0
+        self.eliminacion = False
 
 def main():
     pygame.init()
@@ -161,18 +196,16 @@ def main():
     fondo = load_image("fondo.jpg", IMG_DIR, alpha=False)
     jugador1 = Vicho(40,"plox.gif")
     #jugador2 = Vicho(SCREEN_WIDTH-40,"cc.bmp")
-    #En esta linea hacemos hacer aparecer al jugador de colo colo que aun no tiene implementada alguna IA
     clock = pygame.time.Clock()
     pygame.key.set_repeat(1, 25)  # Activa repeticion de teclas
     pygame.mouse.set_visible(False)
-    contadorBalas = 0
-    balas = []
-    ultimo="r"
+    game = daGame()
     
     # el bucle principal del juego
     try:
         
         while True:
+            game.tiempoActual = time.clock()
             directores = []
             clock.tick(60)
 
@@ -187,50 +220,89 @@ def main():
                 jugador1.rect.y -= 3
                 if not("u" in directores):
                     directores.append("u")
-                ultimo = "u"
+                game.ultimo = "u"
             if pygame.key.get_pressed()[K_DOWN]:
                 jugador1.rect.y += 3
                 if not("d" in directores):
                     directores.append("d")
-                ultimo = "d"
+                game.ultimo = "d"
             if pygame.key.get_pressed()[K_LEFT]:
                 jugador1.rect.x -= 3
                 if not("l" in directores):
                     directores.append("l")
-                ultimo = "l"
+                game.ultimo = "l"
             if pygame.key.get_pressed()[K_RIGHT]:
                 jugador1.rect.x += 3
                 if not("r" in directores):
                     directores.append("r")
-                ultimo = "r"
-            if pygame.key.get_pressed()[K_k] and contadorBalas == 0:
-                d = list(ultimo)
-                contadorBalas +=1
+                game.ultimo = "r"
+            if pygame.key.get_pressed()[K_k] and jugador1.contadorBalas == 0:
+                d = list(game.ultimo)
+                jugador1.contadorBalas +=1
                 if len(directores)>0 and not("l" in directores and "r" in directores) and not("u" in directores and "d" in directores):
-                    balas.append(Bala(jugador1,"bola.png",directores))
+                    game.balas.append(Bala(jugador1,"bola.png",directores))
                 else:
-                    balas.append(Bala(jugador1,"bola.png",d))
-            if contadorBalas>=1:
-                contadorBalas+=1
-            if contadorBalas>=20:
-                contadorBalas=0
+                    game.balas.append(Bala(jugador1,"bola.png",d))
+            if jugador1.contadorBalas>=1:
+                jugador1.contadorBalas+=1
+            if jugador1.contadorBalas>=20:
+                jugador1.contadorBalas=0
                 
+            #numero = uniform(0,1)
+            #game.enemies += 1
+            game.tiempoActual = time.clock()
+            if game.tiempoActual - game.spawn >= 4 and game.enemies<10:
+               game.spawn = game.tiempoActual
+               game.lovers.append(vichoLover("pela.gif"))
+               game.enemies += 1
         
             # actualizamos la pantalla
             screen.blit(fondo, (0, 0))
             screen.blit(jugador1.image, jugador1.rect)
-            if len(balas)>0:
-                for i in range(len(balas)):
-                    balas[i].mover()
-                    if balas[i].mov:
-                        screen.blit(balas[i].image,balas[i].rect)
-            if len(balas)>0:
+
+            
+
+            if len(game.balas)>0:
+                for i in range(len(game.balas)):
+                    game.balas[i].mover()
+                    if game.balas[i].mov:
+                        screen.blit(game.balas[i].image,game.balas[i].rect)
+
+            if len(game.balas)>0:
                 count = 0
-                while count<len(balas):
-                    if balas[0].mov == False:
-                        del balas[0]
+                while count<len(game.balas):
+                    if game.balas[count].mov == False:
+                        del game.balas[count]
                     else:
                         count+=1
+
+            if len(game.lovers)>0:
+                for i in range(len(game.lovers)):
+                    if game.lovers[i].vivo==True:
+                        game.lovers[i].mover(jugador1)
+                        screen.blit(game.lovers[i].image,game.lovers[i].rect)
+
+            if len(game.lovers)>0 and len(game.balas)>0:
+                for i in range(len(game.balas)):
+                    for j in range(len(game.lovers)):
+                        if game.balas[i].tunazo(game.lovers[j]) and game.balas[i].mov==True:
+                            game.balas[i].mov = False
+                            game.balas[i].kill()
+                            game.lovers[j].hp -= 10
+                            if game.lovers[j].hp <= 0:
+                                game.lovers[j].vivo = False
+                                game.lovers[j].kill()
+                                game.enemies -= 1
+                                game.eliminacion = True
+
+            if len(game.lovers)>0 and game.eliminacion:
+                count = 0
+                while count<len(game.lovers):
+                    if game.lovers[count].vivo == False:
+                        del game.lovers[count]
+                    else:
+                        count+=1
+                game.eliminacion = False
 
             #screen.blit(jugador2.image, jugador2.rect)
             #para hacer aparecer al jugador
