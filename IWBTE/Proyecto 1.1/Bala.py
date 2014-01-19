5ea16e582dcf69f2df694875d56cde34a0242521
NevadaStreets <vidominguez@uc.cl>
2014-01-19 19:40:52 -0300

    Mods bala y hp
    :D

---
+++ IWBTE/Proyecto 1.1/Bala.py
@@ -0,0 +1,114 @@
+import pygame
+from pygame.locals import *
+import os
+import sys
+from random import uniform
+from random import randint
+import time
+from math import floor
+
+def load_image(nombre, dir_imagen, alpha=False):
+    """Funcion que retorna una imagen"""
+    # Encontramos la ruta completa de la imagen
+    ruta = os.path.join(dir_imagen, nombre)
+    try:
+        image = pygame.image.load(ruta)
+    except:
+        print ("Error, no se puede cargar la imagen: ", ruta)
+        sys.exit(1)
+    # Comprobar si la imagen tiene "canal alpha" (como los png)
+    if alpha == True:
+        image = image.convert_alpha()
+    else:
+        image = image.convert()
+    return image
+
+class Bala(pygame.sprite.Sprite):
+    def __init__(self, vichopls,dir):
+        pygame.sprite.Sprite.__init__(self)
+        self.frameActual = 1
+        self.recorrido = 0
+        self.image = load_image("Baqueta/Frames/1.gif", "imagenes", alpha=True)
+        self.rect = Rect(0,0,10,10)
+        self.rect.centerx =vichopls.rect.centerx
+        self.rect.centery =vichopls.rect.centery+10
+        self.dir = dir
+        self.mov = True
+
+
+        #Instanciacion y llenado del diccionario
+        self.movimiento = dict()
+        self.movimiento[""] = self.right
+        self.movimiento["u"] = self.up
+        self.movimiento["d"] = self.down
+        self.movimiento["l"] = self.left
+        self.movimiento["r"] = self.right
+        self.movimiento["ur"] = self.upRight
+        self.movimiento["dr"] = self.downRight
+        self.movimiento["dl"] = self.downLeft
+        self.movimiento["ul"] = self.upLeft
+
+    def tunazo(self, baleado):
+        if self.rect.colliderect(baleado.rect):
+            self.kill()
+            self.mov = False
+            return True
+
+    def up(self,reloj):
+        if self.recorrido < 250:
+            self.recorrido += 5.5*(reloj/30)
+            self.rect.centery -= 5.5*(reloj/30)
+        else:
+            self.kill()
+            self.mov = False
+
+    def down(self,reloj):
+        if self.recorrido < 250:
+            self.recorrido += 5.5*(reloj/30)
+            self.rect.centery += 5.5*(reloj/30)
+        else:
+            self.kill()
+            self.mov = False
+
+    def right(self,reloj):
+        if self.recorrido < 250:
+            self.recorrido += 5.5*(reloj/30)
+            self.rect.centerx += 5.5*(reloj/30)
+        else:
+            self.kill()
+            self.mov = False
+
+    def left(self,reloj):
+        if self.recorrido < 250:
+            self.recorrido += 5.5*(reloj/30)
+            self.rect.centerx -= 5.5*(reloj/30)
+        else:
+            self.kill()
+            self.mov = False
+
+    def upRight(self, reloj):
+        self.up(reloj)
+        self.right(reloj)
+
+    def downRight(self,reloj):
+        self.down(reloj)
+        self.right(reloj)
+
+    def downLeft(self,reloj):
+        self.down(reloj)
+        self.left(reloj)
+
+    def upLeft(self,reloj):
+        self.up(reloj)
+        self.left(reloj)
+
+    def cambiarFrame(self):
+        if self.mov == True:
+            self.frameActual += 1
+            if self.frameActual>8:
+                self.frameActual = 1
+            self.image = load_image("Baqueta/Frames/"+str(self.frameActual)+".gif", "imagenes", alpha=True)
+
+    #La funcion mover paso de tener muchas lineas a solo una. Todo auspiciado por Pyhton, yey!
+    def mover(self, reloj):
+        (self.movimiento[self.dir])(reloj)
---
+++ IWBTE/Proyecto 1.1/HP.py
@@ -0,0 +1,25 @@
+import pygame
+from pygame.locals import *
+import os
+import sys
+from random import uniform
+from random import randint
+import time
+from math import floor
+
+class HP(pygame.sprite.Sprite):
+    def __init__(self,_cargar):
+        pygame.sprite.Sprite.__init__(self)
+        self.cargar = _cargar
+        self.image = self.cargar("Barra HP/h100.gif", "imagenes", alpha = True)
+        self.rect = self.image.get_rect()
+        self.rect.centerx=115
+        self.rect.centery=20
+
+    def actualizar(self,jugador):
+        daHP = int(floor(jugador.hp))
+        if daHP<=0:
+            daHP=0
+        if daHP>100:
+            daHP=100
+        self.image = self.cargar("Barra HP/h"+str(daHP)+".gif", "imagenes", alpha = True)
